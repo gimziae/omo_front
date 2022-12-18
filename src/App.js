b352cf86d606ecb36c9e3ec455a383a31eb8ff18
Kim Jiae <95462028+gimziae@users.noreply.github.com>
2022-12-18 23:06:23 +0900

    기본 설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./scss/App.scss";
 
+import styled from "styled-components";
+
+const Headers = styled.h1`
+	color: #fbfe56;
+`;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Headers>OMO main 입니다.</Headers>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/redux/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux"; // 합치는 거
+
+// 내보내기
+export default combineReducers({});
