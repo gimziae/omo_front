d1795a73be72ebb2060b03ca9a8886e51acafa6a
Kim Jiae <95462028+gimziae@users.noreply.github.com>
2022-12-22 21:07:04 +0900

    충돌

--- src/App.js
+++ src/App.js
@@ -6,27 +6,24 @@ import Diary from "./pages/Board/Diary";
 import Calendar from "./pages/Board/Calendar";
 import Calculate from "./pages/Board/Calculate";
 
-
 import Main from "./pages/Main";
 
 import "./scss/style.scss";
 
 function App() {
-
 	return (
 		<div className="App">
 			<Routes>
 				<Route path="/" element={<Main />} />
 				<Route path="/board" element={<Board />} />
-        <Route path="/diary" element={<Diary />} />
-        <Route path="/calender" element={<Calendar />} />
-        <Route path="/calculate" element={<Calculate />} />
+				<Route path="/diary" element={<Diary />} />
+				<Route path="/calender" element={<Calendar />} />
+				<Route path="/calculate" element={<Calculate />} />
 				<Route path="/register" element={<Register />} />
 				<Route path="/login" element={<Login />} />
 			</Routes>
 		</div>
 	);
-
 }
 
 export default App;
