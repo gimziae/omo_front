f9c3b942c0abf7bb3eb8578f37fb5726b0cf42e3
Park-Boyoung <bypark9411@gmail.com>
2022-12-23 21:22:48 +0900

    board

--- src/App.js
+++ src/App.js
@@ -10,22 +10,22 @@ import Register from "./components/Register";
 import Diary from "./pages/Board/Diary";
 import Calendar from "./pages/Board/Calendar";
 import Calculate from "./pages/Board/Calculate";
-import Board from "./pages/Board/Diary";
+import Board from "./pages/Board/Board";
 
 function App() {
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/" element={<Main />} />
-				<Route path="/board" element={<Board />} />
-				<Route path="/diary" element={<Diary />} />
-				<Route path="/calender" element={<Calendar />} />
-				<Route path="/calculate" element={<Calculate />} />
-				<Route path="/register" element={<Register />} />
-				<Route path="/login" element={<Login />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/board" element={<Board />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="/calender" element={<Calendar />} />
+        <Route path="/calculate" element={<Calculate />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
