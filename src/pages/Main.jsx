50542597d7b44e729eb600658dbc0d71c0891f4a
Kim Jiae <95462028+gimziae@users.noreply.github.com>
2022-12-28 22:06:22 +0900

    메인 페이지 섹션 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
+
 import Header from "../components/Header";
 import Best from "../components/main/Best";
-import Intro from "../components/main/Intro";
+
 import Plan from "../components/main/Plan";
 import Popular from "../components/main/Popular";
 
@@ -14,13 +15,13 @@ export default function Main() {
 			<Header />
 			<main>
 				<div className="mainWrap">
-					<Intro />
 					{/* plan 로그인 시에만 보이게 */}
 					{!isLogin ? null : <Plan />}
 					<Popular />
 					<Best />
 				</div>
 			</main>
+			;
 		</>
 	);
 }
