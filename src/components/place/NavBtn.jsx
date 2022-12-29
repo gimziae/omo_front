52235e95bb442cecd5e9a27e26850c97edd7d294
Kim Jiae <95462028+gimziae@users.noreply.github.com>
2022-12-29 18:57:45 +0900

    상세페이지

--- src/components/place/NavBtn.jsx
+++ src/components/place/NavBtn.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { NavLink, useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 const Nav = styled.nav`
 	ul {
@@ -13,8 +13,6 @@ const Nav = styled.nav`
 			align-items: center;
 			width: 180px;
 			height: 40px;
-			background-color: #c79aff;
-			opacity: 0.7;
 			margin: 0 10px;
 			cursor: pointer;
 			&:hover {
@@ -22,8 +20,18 @@ const Nav = styled.nav`
 			}
 
 			a {
+				width: 100%;
+				height: 100%;
+				background-color: #c79aff;
+				opacity: 0.5;
+
 				font-size: 20px;
+				text-align: center;
+				line-height: 40px;
 				color: white;
+				&.active {
+					opacity: 1;
+				}
 			}
 		}
 	}
@@ -36,23 +44,16 @@ export default function NavBtn() {
 		<>
 			<Nav>
 				<ul>
-					{window.location.path === "/tour" ? (
-						<li style={{ background: "#111" }}>
-							<Link to="/tour">관광지</Link>
-						</li>
-					) : (
-						<li>
-							<Link to="/tour">관광지</Link>
-						</li>
-					)}
 					<li>
-						<Link to="/tour">관광지</Link>
+						<NavLink to="/tour" activeClassName="active">
+							관광지
+						</NavLink>
 					</li>
 					<li>
-						<Link to="/culture">문화시설</Link>
+						<NavLink to="/culture">문화시설</NavLink>
 					</li>
 					<li>
-						<Link to="/food">식당</Link>
+						<NavLink to="/food">식당</NavLink>
 					</li>
 				</ul>
 			</Nav>
---
+++ src/pages/DetailView.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Detail from "../components/place/Detail";
+
+export default function DetailView() {
+	return (
+		<>
+			<Header />
+			<Detail />
+			<Footer />
+		</>
+	);
+}
